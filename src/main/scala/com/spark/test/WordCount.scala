8df8e5e984bfa553cc8e97772c0a7c9b08079dcd
GURDIT_SINGH <gurdit.singh.044@gmail.com>
2016-08-21 18:39:08 +0530

    Initial commit

---
+++ src/main/scala/com/spark/test/WordCount.scala
@@ -0,0 +1,22 @@
+package com.spark.test
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+ * Created by GURDIT_SINGH
+ */
+object WordCount {
+  
+  def wordCount(sc:SparkContext,input:String,output:String)={
+    val textFile=sc.textFile(input)
+    val result=textFile.flatMap(line=>line.split(" ")).map(word=>(word,1)).reduceByKey((x,y)=>x+y);
+   result.saveAsTextFile(output)
+  }
+
+  def main(args: Array[String]) {
+    val conf=new SparkConf().setMaster("local").setAppName("WordCount")
+    val sc=new SparkContext(conf)
+    wordCount(sc,args(0),args(1))
+  }
+
+}
